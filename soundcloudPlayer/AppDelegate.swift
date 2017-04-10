a9d4cf875948655656ec2960ad6e0c5a23527221
Horváth Balázs <balazs630@icloud.com>
2017-04-10 22:45:43 +0200

    Fix bug: Unable to reopen application window after closed

--- soundcloudPlayer/AppDelegate.swift
+++ soundcloudPlayer/AppDelegate.swift
@@ -11,15 +11,25 @@ import WebKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    lazy var windows = NSWindow()
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        if !flag {
+            for window in sender.windows {
+                window.makeKeyAndOrderFront(self)
+            }
+        }
+        
+        return true
+    }
+    
 }
-
